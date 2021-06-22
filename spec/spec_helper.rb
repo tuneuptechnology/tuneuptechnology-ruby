9a43f9580bc14182144f02498a73c1e82caaa1d3
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-06-21 20:25:26 -0600

    fix: add single lcov file to coverage results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'vcr'
 require 'simplecov'
 require 'simplecov-lcov'
 
+SimpleCov::Formatter::LcovFormatter.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 
