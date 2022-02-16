8fcf0298c53b3d52466c5215c8406d0530cf37eb
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-02-16 15:20:41 -0700

    fix: HTML coverage report generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,11 @@ require 'vcr'
 require 'simplecov'
 require 'simplecov-lcov'
 
-SimpleCov::Formatter::LcovFormatter.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
 SimpleCov.start
 
 API_EMAIL = ENV['API_EMAIL'] ||= 'mock@example.com'
